67ec2f539c5e92994a4cebed33ff262003c18aac
abelino <abelino.romo@gmail.com>
2025-04-26 17:41:37 -0700

    v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NervesSystemAction.MixProject do
   def project do
     [
       app: :nerve_system_action,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: []
